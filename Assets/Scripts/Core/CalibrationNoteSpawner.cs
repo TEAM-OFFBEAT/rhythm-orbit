e926ec4394827558662e470e1752c0eccba954f0
binpositive <tpqls6382@gmail.com>
2026-07-03 21:08:50 +0900

    feat: 오디오 출력 오프셋 조정 기능 추가

--- Assets/Scripts/Core/CalibrationNoteSpawner.cs
+++ Assets/Scripts/Core/CalibrationNoteSpawner.cs
@@ -3,12 +3,13 @@ using UnityEngine;
 
 public class CalibrationNoteSpawner : SceneSingleton<CalibrationNoteSpawner>
 {
-    private double BeatDuration = RhythmClock.Instance.GetNoteDuration();// RhythmeClock 참조
+    private double BeatDuration; // RhythmeClock 참조
     private const int NoteCount = 4;
 
     /// <summary>120BPM 기준 0.5초 간격의 NoteData 4개를 생성하고 NoteRenderer에 전달해 반환. HUD에서 호출.</summary>
     public List<NoteData> SpawnNotes()
-    {
+    {   
+        BeatDuration = RhythmClock.Instance.GetNoteDuration()*2.0;
         NoteRenderer.Instance.ClearAll();
         double firstJudgeTime = AudioSettings.dspTime + NoteRenderer.Instance.LeadTime;
         var result = new List<NoteData>();
