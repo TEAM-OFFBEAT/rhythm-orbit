716e142f196bfc18b45e777ae030ac8af75860ea
binpositive <tpqls6382@gmail.com>
2026-08-07 22:11:30 +0900

    feat: 사운드 id 목록 enum

---
+++ Assets/Scripts/Core/SoundId.cs
@@ -0,0 +1,30 @@
+public enum BgmId
+{
+    MainMenu,
+    Tutorial,
+    CoreLoop106,
+    CoreLoop120,
+    CoreLoop144,
+    Intro
+}
+
+public enum SfxId
+{
+    ButtonClick,
+    GameWin,
+    GameLose,
+    HappyEnding,
+
+    HitHigh,
+    HitLow,
+    Miss,
+
+    RoundStart1,
+    RoundStart2,
+    RoundStart3,
+
+    TurnSuccess,
+    TurnFail,
+    SceneTransition,
+    GameStart
+}
